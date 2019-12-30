fe7b1b00269d28f80ff3ace354a8875be45103e1
matinbeigi99 <45142752+matinbeigi99@users.noreply.github.com>
2019-12-30 21:24:56 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
-$url = file_get_contents('http://data.alexa.com/data?cli=10&dat=snbamz&url=http://hoshiland.com');
- 
-//Alexa Rank
-preg_match('/\<popularity url\="(.*?)" text\="([0-9]+)" source\="panel"\/\>/si', $url, $matches);
-$globalrank = ($matches[2]) ? $matches[2] : 0;
- 
- preg_match('/\<COUNTRY CODE\="(.*?)" RANK\="([0-9]+)"\/\>/si', $url, $matches);
-$iran = ($matches[2]) ? $matches[2] : 0;
-/*
-<POPULARITY URL="hoshiland.com/" TEXT="870444" SOURCE="panel"/>
-<COUNTRY CODE="IR" NAME="Iran" RANK="12735"/>
-*/
-echo "Global Rate : ".$globalrank;
-echo "Iran Rate : ".$iran;
- 
+
+function GetRankAlexa($site){
+
+    header('Content-Type: text/html; charset=utf-8');
+    $url = file_get_contents("http://data.alexa.com/data?cli=10&dat=snbamz&url=$site");
+    preg_match('/\<popularity url\="(.*?)" text\="([0-9]+)" source\="panel"\/\>/si', $url, $rankglobal);
+    preg_match('/\<COUNTRY CODE\="(.*?)" RANK\="([0-9]+)"\/\>/si', $url, $rankiran);
+    $iran = ($rankiran[2]) ? $rankiran[2] : 0 ;
+    $globalrank = ($rankglobal[2]) ? $rankglobal[2] : 0 ;
+    echo "$site : Global Rate => $globalrank Iran Rate => $iran";
+     
+}
+     
+GetRankAlexa('hoshiland.com');
+
 ?>
